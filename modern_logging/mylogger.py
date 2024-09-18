9e60dc96de6726bb2218a54c1e6de7152ae2a570
PabloFuente14 <pablofuente14@opendeusto.es>
2024-09-18 17:23:53 +0200

    class MyJSON

---
+++ modern_logging/mylogger.py
@@ -0,0 +1,25 @@
+import datetime as dt
+import json
+import logging
+from typing import override
+
+LOG_RECORD_BUILTIN_ATTRS = {}
+
+
+class MyJSONFormatter(logging.Formatter):
+    def __init__(self, *, fmt_keys: dict[str, str] | None = None):
+        super().__init__()
+        self.fmt_keys = fmt_keys if fmt_keys is not None else {}
+
+    @override
+    def format(self, record: logging.LogRecord) -> str:
+        message = self._prepare_log_dict(record)
+        return json.dumps(message, default=str)
+
+    def _prepare_log_dict(self, record:logging.LogRecord):
+        always_fields = {
+            "message": record.getMessage(),
+            "timestamp": dt.datetime.fromtimestamp(
+                record.created, tz=dt.timezone.utc
+            ).isoformat(),
+        }
