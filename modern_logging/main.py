b772bdba0e2f0357c0051a89e5e97a0a07a573a5
PabloFuente14 <pablofuente14@opendeusto.es>
2024-09-23 17:23:28 +0200

    final changes and explanations

--- modern_logging/main.py
+++ modern_logging/main.py
@@ -4,19 +4,19 @@ import logging.handlers
 import pathlib
 
 
-logger = logging.getLogger("myapp")
+logger = logging.getLogger("myapp") #create logger,if not exists
 
 
 def setup_logging():
     config_file = pathlib.Path(r'modern_logging\configs\config1.json')
     with open(config_file) as f_in:
         config = json.load(f_in)
-    logging.config.dictConfig(config)
+    logging.config.dictConfig(config) #charge configuration defined in config file
 
 def main():
-    setup_logging()
+    setup_logging() #se inicializa configuración del logging
     logger.debug("debug message", extra ={'x':'hello'})
-    logger.info("info message")
+    logger.info("info message", extra= {"1":"lpm"})
     logger.warning("warning message")
     logger.error("error message")
     logger.critical("critical message")    
--- modern_logging/mylogger.py
+++ modern_logging/mylogger.py
@@ -32,44 +32,41 @@ LOG_RECORD_BUILTIN_ATTRS = {
 
 
 class MyJSONFormatter(logging.Formatter):
+    #if the formatting keys are not passd, the formatter has no info
     def __init__(self, *, fmt_keys: dict[str, str] | None = None):
-        super().__init__()
+        super().__init__() 
         self.fmt_keys = fmt_keys if fmt_keys is not None else {}
 
-    @override
+    @override #the parent method
     def format(self, record: logging.LogRecord) -> str:
         message = self._prepare_log_dict(record)
         return json.dumps(message, default=str)
 
     def _prepare_log_dict(self, record:logging.LogRecord):
         always_fields = {
-            "message": record.getMessage(),
-            "timestamp": dt.datetime.fromtimestamp(
+            "message": record.getMessage(), ##getting the message from the LogRecord object
+            "timestamp": dt.datetime.fromtimestamp( ##convertinr
                 record.created, tz=dt.timezone.utc
             ).isoformat(),
         }
-        if record.exc_info is not None:
-            always_fields["exc_info"] = self.formatException(record.exc_info)
+        if record.exc_info is not None: #tuple containin exception rised during logging process
+            always_fields["exc_info"] = self.formatException(record.exc_info) #formatting exception
             
-        if record.stack_info is not None:
+        if record.stack_info is not None: #this attb contains the sequence of function calls
             always_fields["stack_info"] = self.formatStack(record.stack_info)
             
-        message = {
+        message = { ##adding to log message other standards passed on the fmt_keys
             key: msg_val
-            if (msg_val := always_fields.pop(val, None)) is not None
-            else getattr(record, val)
+            if (msg_val := always_fields.pop(val, None)) is not None #assings to msg_val the value of the always_field element, returns None if there's nothing
+            else getattr(record, val) #finds fot the vaule at LogRecord istance
             for key, val in self.fmt_keys.items()
         }
         message.update(always_fields)
 
-        for key, val in record.__dict__.items():
-            if key not in LOG_RECORD_BUILTIN_ATTRS:
-                message[key] = val
+        for key, val in record.__dict__.items(): 
+            if key not in LOG_RECORD_BUILTIN_ATTRS: #if key is not a standard key, is an extra and added to message
+                message[key] = val ## getting the extra
 
         return message
 
 
-class NonErrorFilter(logging.Filter):
-    @override
-    def filter(self, record: logging.LogRecord) -> bool | logging.LogRecord:
-        return record.levelno <= logging.INFO
